e22b5461508acd9e05718540625115eba8eb5080
DESKTOP-194V8A0\Thanh Tan <nhthanhtan@gmail.com>
2017-12-06 23:45:37 +0700

    Trần Nhật Tâm
    Trần Nhật Tâm cải tiến chương trình vẽ dùng GDI và ribbon

---
+++ 1512488/Source Code/1512488_GDIPlusRibbon/RibbonIDs.h
@@ -0,0 +1,37 @@
+// *****************************************************************************
+// * This is an automatically generated header file for UI Element definition  *
+// * resource symbols and values. Please do not modify manually.               *
+// *****************************************************************************
+
+#pragma once
+
+#define cmdExit 2 
+#define cmdExit_LabelTitle_RESID 60001
+#define ID_LINE 3 
+#define ID_LINE_LabelTitle_RESID 210
+#define ID_LINE_SmallImages_RESID 212
+#define ID_LINE_LargeImages_RESID 211
+#define ID_RECTANGLE 4 
+#define ID_RECTANGLE_LabelTitle_RESID 213
+#define ID_RECTANGLE_SmallImages_RESID 215
+#define ID_RECTANGLE_LargeImages_RESID 214
+#define ID_ELLIPSE 5 
+#define ID_ELLIPSE_LabelTitle_RESID 216
+#define ID_ELLIPSE_SmallImages_RESID 218
+#define ID_ELLIPSE_LargeImages_RESID 217
+#define ID_SIZE_1 6 
+#define ID_SIZE_1_SmallImages_RESID 220
+#define ID_SIZE_1_LargeImages_RESID 219
+#define ID_SIZE_2 7 
+#define ID_SIZE_2_SmallImages_RESID 222
+#define ID_SIZE_2_LargeImages_RESID 221
+#define ID_SIZE_3 8 
+#define ID_SIZE_3_SmallImages_RESID 224
+#define ID_SIZE_3_LargeImages_RESID 223
+#define Draw 9 
+#define Draw_LabelTitle_RESID 60002
+#define Shape 10 
+#define Shape_LabelTitle_RESID 60003
+#define Size 11 
+#define Size_LabelTitle_RESID 60004
+#define InternalCmd2_LabelTitle_RESID 60005
